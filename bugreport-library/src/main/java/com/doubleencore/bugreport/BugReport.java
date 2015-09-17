62992bf7587d733ff7bf7ed48955420a0a3fd4ca
Chris Mitchell <mitchell.687@gmail.com>
2015-09-17 12:24:45 -0700

    Use compat libraries for permission checks

--- bugreport-library/src/main/java/com/doubleencore/bugreport/BugReport.java
+++ bugreport-library/src/main/java/com/doubleencore/bugreport/BugReport.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.app.Application;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 
 import com.doubleencore.bugreport.internal.BugReportInternal;
 import com.doubleencore.bugreport.internal.ScreenshotObserver;
@@ -42,9 +44,9 @@ public class BugReport {
     }
 
     private static boolean checkPermissions(Activity activity, int requestCode) {
-        if (activity.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
                 PackageManager.PERMISSION_GRANTED) {
-            activity.requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, requestCode);
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, requestCode);
             return false;
         } else {
             return true;
